13dbfe3309bb6366e52abf044b09c55fbf8a6dbb
Alex Hramyko <64614039+Funkylop@users.noreply.github.com>
2021-02-24 10:20:43 +0300

    Delete Homework03 directory

--- Homework03/src/main/java/by/epam_training/homework03/factory/FileAbstractFactory.java
+++
@@ -1,9 +0,0 @@
-package by.epam_training.homework03.factory;
-
-import by.epam_training.homework03.parser.FileParser;
-import by.epam_training.homework03.reader.FileDataReader;
-
-public interface FileAbstractFactory {
-    FileParser getFileParser();
-    FileDataReader getFileDataReader();
-}
--- Homework03/src/main/java/by/epam_training/homework03/reader/FileDataReader.java
+++
@@ -1,7 +0,0 @@
-package by.epam_training.homework03.reader;
-
-import java.util.List;
-
-public interface FileDataReader {
-    List<String> read(String path);
-}
